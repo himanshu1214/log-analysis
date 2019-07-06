f28e954cb2fe14c1ab7f407502de9155ab60bcbb
himanshu1214 <gupta.h@husky.neu.edu>
2019-07-06 18:19:52 -0400

    Revert "removed VIEWS from code"
    This reverts commit eff397df5f4dc1f4df9ba01f70185b9f69ba5720.

--- main.py
+++ main.py
@@ -27,7 +27,9 @@ class Articles:
         :param: takes no parameter
         :return: df: DataFrame of views and Articles
         '''
-        self.conn.execute("SELECT count(*) as views, title FROM log JOIN articles ON log.path = concat('/article/', articles.slug) group by title order by views DESC LIMIT 3;")
+        self.conn.execute("SELECT count(*) as num, A.slug FROM articles AS A \
+        INNER JOIN log AS B ON B.path like '%'|| A.slug || '%' group by A.slug \
+        order by num DESC LIMIT 3;")
         res = self.conn.fetchall()
         df = pd.DataFrame(res, columns = ['Views', 'Articles'])
         # print(res)
@@ -42,9 +44,11 @@ class Articles:
         :param: takes no parameter
         :return: df: Dataframe of Author and Views
         '''
-        self.conn.execute("SELECT A.name, count(*) as views FROM aut_art A \
-        INNER JOIN log AS B ON B.path = concat('/article/', A.slug) group by A.name \
-        order by views DESC LIMIT 3;")
+        self.conn.execute("CREATE OR REPLACE VIEW aut_art AS SELECT A.slug, \
+        A.author, B.name FROM articles AS A, authors AS B WHERE A.author = B.id;")
+        self.conn.execute("SELECT A.name, count(*) as num FROM aut_art A \
+        INNER JOIN log AS B ON B.path like '%'|| A.slug || '%' group by A.name \
+        order by num DESC LIMIT 3;")
         res = self.conn.fetchall()
         df = pd.DataFrame(res, columns = ['Author', 'Views'])
         return df
@@ -58,7 +62,19 @@ class Articles:
         :param: takes no parameter
         :return: df: Dataframe of %errors and Date
         '''
+        view_date_tab = "CREATE OR REPLACE VIEW date_tab AS SELECT \
+        to_char(time, 'yyyy/mm/dd') date_part, status FROM log;"
+        view_group_query = 'CREATE OR REPLACE VIEW group_tab AS SELECT \
+        count(*) as num, date_part, status FROM date_tab group by date_part, status;'
+        view_percent_error_tab = "CREATE OR REPLACE VIEW error_tab AS SELECT \
+        A.num/B.sum*100 error, A.date_part FROM (SELECT num, date_part FROM \
+        group_tab WHERE status='404 NOT FOUND' order by date_part ASC) A, \
+        (SELECT sum(num), date_part FROM group_tab group by date_part \
+        order by date_part) B WHERE A.date_part=B.date_part order by error DESC;"
         select_query = 'SELECT * FROM error_tab WHERE error>1;'
+        self.conn.execute(view_date_tab)
+        self.conn.execute(view_group_query)
+        self.conn.execute(view_percent_error_tab)
         self.conn.execute(select_query)
         res = self.conn.fetchall()
         df=pd.DataFrame(res, columns=['%error', 'date'])
